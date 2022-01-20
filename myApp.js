4ef2caf956e2c786aeeb9c4709cfdeeb6e2d9229
Ethan Vaz <ethanmvaz@gmail.com>
2022-01-20 15:54:08 +0530

    Absolute Path

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-var absolutePath = __dirname + "/views/index.html";
 console.log("Hello World");
-app.get("/",absolutePath);
+app.get("/",function(req,res){
+    res.send(__dirname + "/views/index.html")
+});
 
 
 
