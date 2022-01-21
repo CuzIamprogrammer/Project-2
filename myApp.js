ef307fba9a92f615f36b553ee6ca5ed93a58ad16
Ethan Vaz <ethanmvaz@gmail.com>
2022-01-21 13:22:23 +0530

    shizz

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-require('dotenv').config()
+require('dotenv').config();
 console.log("Hello World");
 app.get("/", function(req, res){
     res.sendFile( __dirname + "/views/index.html");
