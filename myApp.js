b275959bb92da32b300b6d1a2d6571ac5584055f
Ethan Vaz <ethanmvaz@gmail.com>
2022-01-22 16:50:13 +0530

    someShit

--- myApp.js
+++ myApp.js
@@ -9,14 +9,14 @@ app.get("/", function(req, res){
 app.use(express.static(__dirname + "/public"))
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/json", function(req, res){
-    var response  = {"message": "Hello Json"};
+    let response  = {"message": "Hello json"};
     if (process.env.MESSAGE_STYLE === "uppercase"){
-        response.message = "HELLO JSON"; }
+        res.json({"message": "HELLO JSON"}) }
         else {
-            response.message = "Hello Json";
+            res.json(response);
         }
         
-        res.json(response)  })
+         })
         
 
 
